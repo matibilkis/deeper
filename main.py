01e1ecca61bb5d46b92a534b82ed2592bbbfc17e
Matias <matias@GIQ-GOLEM.local>
2020-03-02 14:05:53 +0100

    long

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from nets import Q1
 import random
 
 
-def real_training(run_id, number_betas=10, lr = 10**-2, T=1000, batch_size=1000, min_ep=1):
+def real_training(run_id, number_betas=10, lr = 10**-2,
+ states_wasted=10**4, min_ep=1, batch_size=10, epochs=10):
 
     q1=Q1()
     optimizer = tf.keras.optimizers.Adam(lr = lr)
@@ -23,8 +24,8 @@ def real_training(run_id, number_betas=10, lr = 10**-2, T=1000, batch_size=1000,
     ntable=np.zeros(len(betas))
 
     optimal = max(ps(betas))
-    buffer = ReplayBuffer(buffer_size=T)
-    for episode in tqdm(range(T)):
+    buffer = ReplayBuffer(buffer_size=states_wasted)
+    for episode in tqdm(range(states_wasted)):
         ep = max(np.exp(-episode/100),min_ep)
         label, beta = greedy_action(q1, betas, ep)
         ntable[label]+=1
@@ -32,25 +33,33 @@ def real_training(run_id, number_betas=10, lr = 10**-2, T=1000, batch_size=1000,
         rt.append(reward)
         rts.append(np.sum(rt))
         buffer.add(beta, reward)
-        if (episode > batch_size)&(episode%batch_size==0):
-            actions_did, rewards = buffer.sample(batch_size)
-            with tf.device("/cpu:0"):
-                with tf.GradientTape() as tape:
-                    tape.watch(q1.trainable_variables)
-                    predictions = q1(np.expand_dims(np.array(actions_did),axis=1))
-                    pt.append(ps(greedy_action(q1,betas,ep=0)[1]))
-                    loss_sum = tf.keras.losses.MSE(predictions,np.expand_dims(np.array(rewards),axis=1))
-                    loss = tf.reduce_mean(loss_sum)
-                    grads = tape.gradient(loss, q1.trainable_variables)
-                    optimizer.apply_gradients(zip(grads, q1.trainable_variables))
+        #
+        # pt = train_net(episode,pt, network=q1, optimizer=optimizer, buffer=buffer, epochs=1, batch_size=10)
+        if (episode > batch_size):
+            for k in range(epochs): #loop epoch
+                actions_did, rewards = buffer.sample(batch_size)
+                with tf.device("/cpu:0"):
+                    with tf.GradientTape() as tape:
+                        tape.watch(q1.trainable_variables)
+                        predictions = q1(np.expand_dims(np.array(actions_did),axis=1))
+                        loss_sum = tf.keras.losses.MSE(predictions,np.expand_dims(np.array(rewards),axis=1))
+                        loss = tf.reduce_mean(loss_sum)
+                        grads = tape.gradient(loss, q1.trainable_variables)
+                        optimizer.apply_gradients(zip(grads, q1.trainable_variables))
+                        pt.append(ps(greedy_action(q1,betas,ep=0)[1]))
+
         else:
             pt.append(0.5)
+
+
+
+
     rtsum = rts/np.arange(1,T+1)
     predictions = q1.prediction(betas)
     plot_evolution(rt=rtsum, pr=pt, optimal=optimal, betas=betas, preds=predictions , run_id= run_id)
 
-    data = "betas: " +str(betas)+"\nBernoulli rewards!\nOptimizer: "+optimizer.__str__()+"\nbuffer_batch_size: "+str(batch_size)
-    data = data + "\nLearning rate: "+str(lr)+"\nEpsilon decaying! e^{-t/100}" + "\nBuffer size: "+str(buffer.buffer_size)+"\nMin_ep: "+str(min_ep)
+    data = "betas: " +str(betas)+"\nBernoulli rewards!\nOptimizer: "+optimizer.__str__()+"\nBatch_size: "+str(batch_size)
+    data = data + "\nLearning rate: "+str(lr)+"\nEpsilon decaying! e^{-t/100}" + "\nBuffer size: "+str(buffer.buffer_size)+"\nMin_ep: "+str(min_ep)+"\nEpochs: "+str(epocs)
 
     os.chdir(run_id)
     with open("info.txt", 'w') as f:
@@ -66,4 +75,4 @@ os.chdir("results")
 
 run_id=record()
 number_run = "run_"+str(run_id)
-real_training(number_run, T=10**2)
+real_training(number_run, states_wasted=2*10**4)
--- nets.py
+++ nets.py
@@ -2,21 +2,22 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense
 tf.keras.backend.set_floatx('float64')
 import numpy as np
+# from misc import *
 
 class Q1(tf.keras.Model):
     def __init__(self):
         super(Q1,self).__init__()
 
-        self.l1 = Dense(10, input_shape=(1,),kernel_regularizer=tf.keras.regularizers.l1(0.01),
-    activity_regularizer=tf.keras.regularizers.l2(0.01))
-        self.l2 = Dense(33, kernel_regularizer=tf.keras.regularizers.l1(0.01),
-    activity_regularizer=tf.keras.regularizers.l2(0.01))
-        self.l3 = Dense(10, kernel_regularizer=tf.keras.regularizers.l1(0.01),
-    activity_regularizer=tf.keras.regularizers.l2(0.01))
-        self.l4 = Dense(33, kernel_regularizer=tf.keras.regularizers.l1(0.01),
-    activity_regularizer=tf.keras.regularizers.l2(0.01))
-        self.l5 = Dense(1, kernel_regularizer=tf.keras.regularizers.l1(0.01),
-    activity_regularizer=tf.keras.regularizers.l2(0.01))
+        self.l1 = Dense(10, input_shape=(1,),kernel_regularizer=tf.keras.regularizers.l1(0.05),
+    activity_regularizer=tf.keras.regularizers.l2(0.05))
+        self.l2 = Dense(33, kernel_regularizer=tf.keras.regularizers.l1(0.05),
+    activity_regularizer=tf.keras.regularizers.l2(0.05))
+        self.l3 = Dense(10, kernel_regularizer=tf.keras.regularizers.l1(0.05),
+    activity_regularizer=tf.keras.regularizers.l2(0.05))
+        self.l4 = Dense(33, kernel_regularizer=tf.keras.regularizers.l1(0.05),
+    activity_regularizer=tf.keras.regularizers.l2(0.05))
+        self.l5 = Dense(1, kernel_regularizer=tf.keras.regularizers.l1(0.05),
+    activity_regularizer=tf.keras.regularizers.l2(0.05))
 
     def call(self, input):
         feat = tf.nn.relu(self.l1(input))
@@ -29,3 +30,22 @@ class Q1(tf.keras.Model):
     def prediction(self, betas):
         inp = np.expand_dims(betas, axis=1)
         return np.squeeze(self(inp).numpy())
+#
+# def train_net(episode,  pt, network,optimizer, buffer,epochs=10, batch_size=10):
+#     pt = pt.copy()
+#     if (episode > batch_size):
+#         for k in range(epochs): #loop epoch
+#             actions_did, rewards = buffer.sample(batch_size)
+#             with tf.device("/cpu:0"):
+#                 with tf.GradientTape() as tape:
+#                     tape.watch(network.trainable_variables)
+#                     predictions = network(np.expand_dims(np.array(actions_did),axis=1))
+#                     loss_sum = tf.keras.losses.MSE(predictions,np.expand_dims(np.array(rewards),axis=1))
+#                     loss = tf.reduce_mean(loss_sum)
+#                     grads = tape.gradient(loss, network.trainable_variables)
+#                     optimizer.apply_gradients(zip(grads, network.trainable_variables))
+#                     pt.append(ps(greedy_action(network,betas,ep=0)[1]))
+#
+#     else:
+#         pt.append(0.5)
+#     return pt
