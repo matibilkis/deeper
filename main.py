4d07aadc1b721073abc110c801aec2b4222e6e34
cooper-cooper <matiasbilkis@gmail.com>
2020-03-03 10:28:42 +0100

    noot working

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import random
 
 
 def real_training(run_id, number_betas=10, lr = 10**-2,
- states_wasted=10**4, min_ep=1, batch_size=10, epochs=10):
+ states_wasted=10**3, min_ep=1, batch_size=10, epochs=10, buffer_size=10**3):
 
     q1=Q1()
     optimizer = tf.keras.optimizers.Adam(lr = lr)
@@ -24,7 +24,7 @@ def real_training(run_id, number_betas=10, lr = 10**-2,
     ntable=np.zeros(len(betas))
 
     optimal = max(ps(betas))
-    buffer = ReplayBuffer(buffer_size=states_wasted)
+    buffer = ReplayBuffer(buffer_size=buffer_size)
     for episode in tqdm(range(states_wasted)):
         ep = max(np.exp(-episode/100),min_ep)
         label, beta = greedy_action(q1, betas, ep)
@@ -33,8 +33,7 @@ def real_training(run_id, number_betas=10, lr = 10**-2,
         rt.append(reward)
         rts.append(np.sum(rt))
         buffer.add(beta, reward)
-        #
-        # pt = train_net(episode,pt, network=q1, optimizer=optimizer, buffer=buffer, epochs=1, batch_size=10)
+
         if (episode > batch_size):
             for k in range(epochs): #loop epoch
                 actions_did, rewards = buffer.sample(batch_size)
@@ -52,14 +51,12 @@ def real_training(run_id, number_betas=10, lr = 10**-2,
             pt.append(0.5)
 
 
-
-
-    rtsum = rts/np.arange(1,T+1)
+    rtsum = rts/np.arange(1,states_wasted+1)
     predictions = q1.prediction(betas)
-    plot_evolution(rt=rtsum, pr=pt, optimal=optimal, betas=betas, preds=predictions , run_id= run_id)
+    plot_evolution(rt=rtsum, pt=pt, optimal=optimal, betas=betas, preds=predictions , run_id= run_id)
 
     data = "betas: " +str(betas)+"\nBernoulli rewards!\nOptimizer: "+optimizer.__str__()+"\nBatch_size: "+str(batch_size)
-    data = data + "\nLearning rate: "+str(lr)+"\nEpsilon decaying! e^{-t/100}" + "\nBuffer size: "+str(buffer.buffer_size)+"\nMin_ep: "+str(min_ep)+"\nEpochs: "+str(epocs)
+    data = data + "\nLearning rate: "+str(lr)+"\nEpsilon decaying! e^{-t/100}" + "\nBuffer size: "+str(buffer.buffer_size)+"\nMin_ep: "+str(min_ep)+"\nEpochs: "+str(epochs)
 
     os.chdir(run_id)
     with open("info.txt", 'w') as f:
@@ -75,4 +72,25 @@ os.chdir("results")
 
 run_id=record()
 number_run = "run_"+str(run_id)
-real_training(number_run, states_wasted=2*10**4)
+
+
+# run_id, number_betas=10, lr = 10**-2,
+#  states_wasted=10**4, min_ep=1, batch_size=10, epochs=10):
+
+real_training(run_id=number_run, lr=10**-2,
+ states_wasted=10**4, min_ep=1, batch_size=5, epochs=1, buffer_size=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####
